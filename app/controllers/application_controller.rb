1e35dc511ae5ed08489ab9e8b15fbb43f4217244
Griffin Dooley <dooleygriffin@gmail.com>
2022-07-14 10:56:30 -0400

    adding more CRUD features

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,39 +8,55 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  # READ all Playlist instances
   get '/browse_playlists' do
     playlists = Playlist.all
     playlists.to_json
   end
 
+  # READ specific Playlist instances
   get '/user_playlists/:id' do
     playlists = User.find(params[:id]).playlists
     playlists.to_json
   end
 
+  # READ songs within a Playlist instance
   get '/playlist/:id' do
     songs_in_playlist = Playlist.find(params[:id]).songs
     songs_in_playlist.to_json
   end
 
+  # CREATE a new Playlist instance
   post '/playlist' do
     Playlist.create(name: params[:name], id: params[:id], rating: params[:rating], user_id: params[:user_id])
   end
 
+  # DELETE a Playlist instance
   delete '/playlist/:id' do
     playlist = Playlist.find(params[:id])
     playlist.destroy
     playlist.to_json
   end
 
-  post '/playlists/:id' do
-    song = Song.create(name: params[:name], artist: params[:artist])
-
-    playlist = Song.find(params[:id])
-    
-    Join.create(song_id: song.id, playlist_id: playlist.id)
+  # UPDATE the name of a Playlist instance
+  patch '/playlist/:id' do
+    playlist = Playlist.find(params[:id])
+    playlist.update(name: params[:name])
+    playlist.to_json
   end
 
+  # CREATE a new User instance
+  post '/new_user' do
+    new_user = User.create(email: params[:email], password: params[:password])
+    new_user.to_json
+  end
+
+# UPDATE email and password of a User instance
+  patch '/user/:id' do
+    user = User.find(params[:id])
+    user.update(email: params[:email], password: params[:password])
+    user.to_json
+  end
 
 
 end
